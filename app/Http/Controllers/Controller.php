7a0db9f3f7e051adbb759887705de089ff802833
simetria-studio <fecruz2022@gmail.com>
2024-10-01 21:06:58 -0300

    .

--- app/Http/Controllers/Controller.php
+++ app/Http/Controllers/Controller.php
@@ -18,9 +18,15 @@ class Controller extends BaseController
 
     public function __construct()
     {
-        $this->url_asaas = 'https://sandbox.asaas.com/api/v3';
-        $this->access_token = '$aact_YTU5YTE0M2M2N2I4MTliNzk0YTI5N2U5MzdjNWZmNDQ6OjAwMDAwMDAwMDAwMDAwODE1ODk6OiRhYWNoXzdmYzRhNzJkLTM5NjUtNDY3Yy05ZGMzLWQ2YjVmY2ZjZWQ5YQ==';
-        $this->wallet_id = '9281f252-4c92-4fab-b560-65ef47059c31';
+        if (url('') == 'https://feitoporbiguacu.com') {
+            $this->url_asaas = 'https://api.asaas.com/v3';
+            $this->access_token = '$aact_YTU5YTE0M2M2N2I4MTliNzk0YTI5N2U5MzdjNWZmNDQ6OjAwMDAwMDAwMDAwMDA0NDYzMjI6OiRhYWNoXzVjYmQ4MGI4LWM1MTctNDc0Zi1hNjVlLTMxOGI4MDZhOGE0YQ==';
+            $this->wallet_id = 'e043a22b-1ceb-42c5-8e8a-9489ea68b9c9';
+        } else {
+            $this->url_asaas = 'https://sandbox.asaas.com/api/v3';
+            $this->access_token = '$aact_YTU5YTE0M2M2N2I4MTliNzk0YTI5N2U5MzdjNWZmNDQ6OjAwMDAwMDAwMDAwMDAwODE1ODk6OiRhYWNoXzdmYzRhNzJkLTM5NjUtNDY3Yy05ZGMzLWQ2YjVmY2ZjZWQ5YQ==';
+            $this->wallet_id = '9281f252-4c92-4fab-b560-65ef47059c31';
+        }
     }
 
     public function dataConfig(Request $request, $data_type)
